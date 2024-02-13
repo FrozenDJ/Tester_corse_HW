eb24bd357b028f5e8f4a7a705d4319e80cf2ab66
Roman_Konkov <roman.konkoff@gmail.com>
2024-02-13 15:40:35 +0400

    create and make task in seminar

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите ваше имя: ')
+    print('Красивое имя!;)')
+    return name
+
+def surname_data():
+    surname = input('Введите вашу фамилию: ')
+    print('Интересная фамилия!;)')
+    return surname
+
+def phone_data():
+    phone = input('Введите ваш номер телефона: ')
+    print('Хороший номер!;)')
+    return phone
+
+def address_data():
+    address = input('Введите ваш адрес проживания: ')
+    print('Неплохой район!;)')
+    return address
