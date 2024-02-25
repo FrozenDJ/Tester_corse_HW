5b0d61ea9bdeaec95f2c02ab8806046c5eb63597
Roman_Konkov <roman.konkoff@gmail.com>
2024-02-25 14:40:27 +0400

    edited ui file, print_data function

--- logger.py
+++ logger.py
@@ -23,18 +23,23 @@ def input_data():
 
 
 def print_data():
-    print('Вывожу данные из 1-го файла: \n')
-    with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
-        data_first = f.readlines()
-        data_first_list = []
-        j = 0
-        for i in range(len(data_first)):
-            if data_first[i] == '\n' or i == len(data_first) - 1:
-                data_first_list.append(''.join(data_first[j: i + 1]))
-                j = i
-        print(''.join(data_first_list))
-
-    print('Вывожу данные из 2-го файла: \n')
-    with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
-        data_second = f.readlines()
-        print(*data_second)
+    variant = int(input(f'В каком варианте вывести данные? 1 или 2? Выберите вариант:'))
+    while variant != 1 and variant != 2:
+        print('Неправильный ввод!')
+        variant = int(input('Выберите вариант: '))
+    if variant == 1:
+        print('Вывожу данные из 1-го файла: \n')
+        with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+            data_first_list = []
+            j = 0
+            for i in range(len(data_first)):
+                if data_first[i] == '\n' or i == len(data_first) - 1:
+                    data_first_list.append(''.join(data_first[j: i + 1]))
+                    j = i
+            print(''.join(data_first_list))
+    else:
+        print('Вывожу данные из 2-го файла: \n')
+        with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+            data_second = f.readlines()
+            print(*data_second)
