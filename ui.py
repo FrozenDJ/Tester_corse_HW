e7a04c0486514c3ff59c7d7a48829192aeb5a758
Roman_Konkov <roman.konkoff@gmail.com>
2024-02-25 15:17:24 +0400

    final commit-3

---
+++ ui.py
@@ -0,0 +1,27 @@
+from logger import input_data, print_data, modify_data, delete_data
+
+def interface():
+    while True:
+        print('Добрый день! Вы попали на специальный бот-справочник от ГБ!: \n 1 - запись данных \n 2 - вывод данных \n 3 - изменение данных \n 4 - удаление данных \n 5 - выход')
+        command = int(input('Введите число: '))
+
+        while command not in [1, 2, 3, 4, 5]:
+            print('Неправильный ввод!')
+            command = int(input('Введите число: '))
+    
+        if command == 1:
+            input_data()
+        elif command == 2:
+            print_data()
+        elif command == 3:
+            modify_data()
+        elif command == 4:
+            delete_data()
+        elif command == 5:
+            print('До свидания!')
+            return
+
+        repeat = input('Хотите продолжить работу? (да/нет): ')
+        if repeat.lower() != 'да':
+            print('До свидания!')
+            return
